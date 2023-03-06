5b6471531898a21d37b1ed8105848bfbb4b1f370
godra88 <ivanfabri88@gmail.com>
2023-03-06 16:05:52 +0100

    Add files via upload

--- GalleryApp/Main/SceneDelegate.swift
+++ GalleryApp/Main/SceneDelegate.swift
@@ -17,8 +17,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         
-       // ConnectionManager.shared.observeReachability()
-    
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
