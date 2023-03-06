6a8416383a703c57111b85e57d42013ab2da4d05
godra88 <ivanfabri88@gmail.com>
2023-03-06 09:24:58 +0100

    Add files via upload

---
+++ GalleryApp/GalleryApp/Main/App.swift
@@ -0,0 +1,13 @@
+//
+//  App.swift
+//  GalleryApp
+//
+//  Created by Ivan Fabri on 03/03/2023.
+//
+
+import Foundation
+
+public class App {
+    static var baseUrl = "https://jsonplaceholder.typicode.com"
+    static var galleryCell = "galleryCell"
+}
---
+++ GalleryApp/GalleryApp/Network/ConnectionManager.swift
@@ -0,0 +1,27 @@
+//
+//  ConnectionManager.swift
+//  GalleryApp
+//
+//  Created by Ivan Fabri on 06/03/2023.
+//
+
+import Foundation
+
+public class ConnectionManager {
+    public static let shared = ConnectionManager()
+    private var reachability: Reachability?
+    
+    public func observeReachability() {
+        do {
+            self.reachability = try Reachability()
+            try self.reachability?.startNotifier()
+        }
+        catch(let error) {
+            print("Error occured while starting reachability notifications : \(error.localizedDescription)")
+        }
+    }
+    
+    public func stopObservingReachability() {
+        reachability?.stopNotifier()
+    }
+}
